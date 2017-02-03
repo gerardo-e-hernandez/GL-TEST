6bf549650759446200eac0932ae33e933d89d23e
Gera <gerardo.e.hernandez01@utrgv.edu>
2017-02-03 00:03:24 -0600

    it now shows the triangle,

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -4,19 +4,22 @@ using namespace std;
 
 void Init()
 {
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glVertex2f(0.0, 0.0);
+	glVertex2f(0.5, 0.0);
+	glVertex2f(0.5, 0.5);
+	glVertex2f(0.0, 0.5);
+	glEnd();
+	glFlush();
 }
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(900, 900);
-	glutInitWindowPosition(200, 100);
+	glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Test Program");
 	glutDisplayFunc(Init);
 	glutMainLoop();
